e0fe6c9d81b8f715d11df5c109813dcc2c47bb94
Sarah Stockton <sstockton@mail.wlu.edu>
2025-11-19 14:02:30 -0500

    Made the font size bigger for the sightings text on the visualizations page

--- ui.R
+++ ui.R
@@ -238,7 +238,8 @@ navbarPage(
                  div(style = "background-color: #2c3e50; padding: 30px; border-radius: 12px; box-shadow: 0 8px 32px rgba(0,0,0,0.2);",
                      h4("Click on top of any bar to see the number of sightings", style = "color: #f9ca24; text-align: center;"),
                      plotOutput("selectedPlot", click = "plots_click"),
-                     verbatimTextOutput("plots_text")
+                     tags$style("#plots_text { font-size: 16px; color: white; }"),
+                     textOutput("plots_text")
                  )
              )
            )
